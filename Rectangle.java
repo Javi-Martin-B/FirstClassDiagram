02eb7081033c618d50938baec7bd385b3fd74214
Javi <jmartinb@inscastellet.cat>
2024-04-26 14:28:22 +0200

    task done

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+    private int positionX;
+    private int positionY;
+    private int height;
+    private int width;
+    private int thicknessLine;
+
+    public Rectangle(int positionX, int positionY, int height, int width, int thicknessLine) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.height = height;
+        this.width = width;
+        this.thicknessLine = thicknessLine;
+    }
+
+    public void Draw() {
+        System.out.println("Dibujando un rectángulo en posición (" + positionX + ", " + positionY + ") con altura " + height + ", ancho " + width + " y grosor de línea " + thicknessLine);
+    }
+}
